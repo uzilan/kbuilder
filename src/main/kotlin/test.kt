2a40c8cf651e03a7d7db710c798f9f56ae89275c
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-10-05 07:21:47 +0200

    trying stuff with outer builder class

--- src/main/kotlin/test.kt
+++ src/main/kotlin/test.kt
@@ -7,12 +7,25 @@ fun main(args: Array<String>) {
     val cat = Cat.build().name("Fluff").lives(9).build()
     print(cat)
 
-    createBuilder()
-
-
+    val dog = DogBuilder.builder().name("Siri").barksalot(true).build()
 }
 
 data class Dog(val name: String, val barksalot: Boolean)
+
+class DogBuilder private constructor() {
+    internal var name: String = ""
+    internal var barksalot: Boolean = false
+
+    companion object {
+        fun builder() = DogBuilder()
+    }
+
+    fun name(name: String): DogBuilder = apply { this.name = name }
+
+    fun barksalot(barksalot: Boolean): DogBuilder = apply { this.barksalot = barksalot }
+
+    fun build() = Dog(name = this.name, barksalot = this.barksalot)
+}
 
 fun createBuilder() {
     val companion = TypeSpec.companionObjectBuilder()
